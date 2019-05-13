4c9635135e19aa283169867ab9996a22ec401434
unknown <Administrator@LSCN730090.china.bmw.corp>
2019-05-13 18:10:08 +0800

    add stream grpc demo

--- advancedgolang/ch4/4-4/4-4service.go
+++ advancedgolang/ch4/4-4/4-4service.go
@@ -1,12 +1,48 @@
 package main
 
 import (
-	"context"
+	context "context"
+	"io"
+	"log"
+	"net"
+
+	hello "work/golang-test/advancedgolang/ch4/4-4/HelloService"
+
+	grpc "google.golang.org/grpc"
 )
 
 type HelloServiceImpl struct{}
 
-func (p *HelloServiceImpl) Hello(ctx context.Context, args *String) (*String, error) {
-	reply := &String{value: "hello:" + args.GetValue()}
+func (p *HelloServiceImpl) Hello(ctx context.Context, args *hello.String) (*hello.String, error) {
+	reply := &hello.String{Value: "hello:" + args.GetValue()}
 	return reply, nil
+}
+
+func (p *HelloServiceImpl) Channel(stream hello.HelloService_ChannelServer) error {
+	for {
+		args, err := stream.Recv()
+		if err != nil {
+			if err == io.EOF {
+				return nil
+			}
+			return err
+		}
+		reply := &hello.String{Value: "hello : " + args.GetValue()}
+
+		err = stream.Send(reply)
+		if err != nil {
+			return err
+		}
+	}
+}
+
+func main() {
+	grpcServer := grpc.NewServer()
+	hello.RegisterHelloServiceServer(grpcServer, new(HelloServiceImpl))
+
+	lis, err := net.Listen("tcp", ":1234")
+	if err != nil {
+		log.Fatal(err)
+	}
+	grpcServer.Serve(lis)
 }
