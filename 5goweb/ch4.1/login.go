6b7342392ef1a6ad73ddd6b4d6fb539f388b274e
unknown <Administrator@LSCN730090.china.bmw.corp>
2019-06-17 14:24:12 +0800

    add upload

--- 5goweb/ch4.1/login.go
+++ 5goweb/ch4.1/login.go
@@ -5,8 +5,10 @@ import (
 	"encoding/hex"
 	"fmt"
 	"html/template"
+	"io"
 	"log"
 	"net/http"
+	"os"
 	"strconv"
 	"strings"
 	"time"
@@ -68,9 +70,38 @@ func login(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func upload(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("method:", r.Method)
+	if r.Method == "GET" {
+		t, _ := template.ParseFiles("upload.gtpl")
+		crutime := time.Now().Nanosecond()
+		h := md5.New()
+		h.Write([]byte(strconv.FormatInt(int64(crutime), 10)))
+		token := hex.EncodeToString(h.Sum(nil))
+		log.Println(t.Execute(w, token))
+	} else {
+		r.ParseMultipartForm(32 << 20)
+		file, handler, err := r.FormFile("uploadfile")
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		defer file.Close()
+		fmt.Fprintf(w, "%v", handler.Header)
+		f, err := os.OpenFile("./test/"+handler.Filename, os.O_WRONLY|os.O_CREATE, 0666)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		defer f.Close()
+		io.Copy(f, file)
+	}
+}
+
 func main() {
 	http.HandleFunc("/", sayhelloName)
 	http.HandleFunc("/login", login)
+	http.HandleFunc("/upload", upload)
 	err := http.ListenAndServe(":9090", nil)
 	if err != nil {
 		log.Fatal("listenandserve:", err)
