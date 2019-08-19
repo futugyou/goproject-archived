ea630611e5579b306aaa615d679f92f1e54de2da
unknown <Administrator@LSCN730090.china.bmw.corp>
2019-08-19 18:03:08 +0800

    http demo

--- 9goweb/ch1/1hello.go
+++ 9goweb/ch1/1hello.go
@@ -3,13 +3,26 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"reflect"
+	"runtime"
 )
 
 func handler(writer http.ResponseWriter, request *http.Request) {
 	fmt.Fprintf(writer, "hello, %s!", request.URL.Path[1:])
 }
+func handler1(writer http.ResponseWriter, request *http.Request) {
+	fmt.Fprintf(writer, "hello1, %s!", request.URL.Path[1:])
+}
 
+func log(h http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		name := runtime.FuncForPC(reflect.ValueOf(h).Pointer()).Name
+		fmt.Println("handler funcation called - ", name)
+		h(w, r)
+	}
+}
 func main() {
-	http.HandleFunc("/", handler)
+	http.HandleFunc("/sss/", handler1)
+	http.HandleFunc("/", log(handler))
 	http.ListenAndServe(":8080", nil)
 }
