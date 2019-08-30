8daa620f2b34a0f963db5db65fb038d07bc377d7
unknown <Administrator@LSCN730090.china.bmw.corp>
2019-08-30 18:01:32 +0800

    Create 3psqldemo.go

---
+++ 9goweb/ch6/3psqldemo.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/lib/pq"
+)
+
+type Post struct {
+	Id      int
+	Content string
+	Author  string
+}
+
+var db *sql.DB
+
+func init() {
+	var err error
+	db, err = sql.Open("postgres", "user=gwp dbname=gwp password=qwp sslmode=disable")
+	if err != nil {
+		panic(err)
+	}
+}
+
+func Posts(limit int) (posts []Post, err error) {
+	rows, err := db.Query("select id,content,author from posts limit $1", limit)
+	if err != nil {
+		return
+	}
+	for rows.Next() {
+		post := Post{}
+		err = rows.Scan(&post.Id, &post.Content, &post.Author)
+		if err != nil {
+			return
+		}
+		posts = append(posts, post)
+	}
+	rows.Close()
+	return
+}
+
+func GetPost(id int) (post Post, err error) {
+	post = Post{}
+	err = db.QueryRow("select id ,content,author from posts where id =$1", id).Scan(&post.Id, &post.Content, &post.Author)
+	return
+}
+
+func (post *Post) Create() (err error) {
+	statement := "insert into posts (content,author) values ($1,$2) returning id"
+	stmt, err := db.Prepare(statement)
+	if err != nil {
+		return
+	}
+	defer stmt.Close()
+	err = stmt.QueryRow(post.Content, post.Author).Scan(&post.Id)
+	return
+}
+func main() {
+	post := Post{Content: "hello 1", Author: "bb"}
+	fmt.Println(post.Create())
+	fmt.Println(post)
+	readPost, _ := GetPost(post.Id)
+	fmt.Println(readPost)
+}
