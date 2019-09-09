9c2eb1ec156d3b124507c3f7ebc22e088b9c49cb
unknown <Administrator@LSCN730090.china.bmw.corp>
2019-09-09 15:16:25 +0800

    add json demo
    add json demo

---
+++ 9goweb/ch7/3json.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+)
+
+type Post struct {
+	Id       int       `json:"id"`
+	Content  string    `json:"content"`
+	Author   Author    `json:"author"`
+	Comments []Comment `json:"comments"`
+}
+
+type Author struct {
+	Id   int    `json:"id"`
+	Name string `json:"name"`
+}
+
+type Comment struct {
+	Id      int    `json:"id"`
+	Content string `json:"content"`
+	Author  string `json:"author"`
+}
+
+func main() {
+	post := Post{
+		Id:      1,
+		Content: "Hello",
+		Author: Author{
+			Id:   2,
+			Name: "same",
+		},
+		Comments: []Comment{
+			Comment{
+				Id:      3,
+				Content: "A",
+				Author:  "AA",
+			},
+			Comment{
+				Id:      4,
+				Content: "B",
+				Author:  "BB",
+			},
+		},
+	}
+
+	output, err := json.MarshalIndent(&post, "", "\t\t")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	err = ioutil.WriteFile("post.json", output, 0644)
+	if err != nil {
+		fmt.Println(err)
+	}
+}
