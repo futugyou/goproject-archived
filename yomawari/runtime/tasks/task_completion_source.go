ea0643eea899b62e78911da7cddfbf80215e7ec6
futugyou <17871902+futugyou@users.noreply.github.com>
2026-08-02 22:04:52 +0800

    Updated TaskCompletionSource

--- yomawari/runtime/tasks/task_completion_source.go
+++ yomawari/runtime/tasks/task_completion_source.go
@@ -6,36 +6,36 @@ import (
 )
 
 type TaskCompletionSource[T any] struct {
-	ctx          context.Context
-	cancelFunc   context.CancelFunc
-	responseChan chan T
-	err          error
-	once         sync.Once
-	setCalled    chan struct{}
+	ctx        context.Context
+	cancelFunc context.CancelFunc
+
+	done chan struct{} // 仅用于通知完成（完成时 close(done)）
+	once sync.Once
+
+	result T
+	err    error
 }
 
 func NewTaskCompletionSource[T any](ctx context.Context, cancelFunc context.CancelFunc) *TaskCompletionSource[T] {
 	return &TaskCompletionSource[T]{
-		ctx:          ctx,
-		cancelFunc:   cancelFunc,
-		responseChan: make(chan T, 1),
-		setCalled:    make(chan struct{}),
+		ctx:        ctx,
+		cancelFunc: cancelFunc,
+		done:       make(chan struct{}),
 	}
 }
 
 func NewFuture[T any](ctx context.Context) (Future[T], Resolver[T]) {
 	ctx, cancel := context.WithCancel(ctx)
 	tcs := &TaskCompletionSource[T]{
-		ctx:          ctx,
-		cancelFunc:   cancel,
-		responseChan: make(chan T, 1),
-		setCalled:    make(chan struct{}),
+		ctx:        ctx,
+		cancelFunc: cancel,
+		done:       make(chan struct{}),
 	}
 	return tcs, tcs
 }
 
 func (tcs *TaskCompletionSource[T]) Done() <-chan struct{} {
-	return tcs.setCalled
+	return tcs.done
 }
 
 func (tcs *TaskCompletionSource[T]) Context() context.Context {
@@ -43,42 +43,64 @@ func (tcs *TaskCompletionSource[T]) Context() context.Context {
 }
 
 func (tcs *TaskCompletionSource[T]) SetResult(result T) {
-	tcs.once.Do(func() {
-		tcs.responseChan <- result
-		close(tcs.setCalled)
-	})
+	if !tcs.TrySetResult(result) {
+		panic("TaskCompletionSource has already been completed")
+	}
 }
 
 func (tcs *TaskCompletionSource[T]) SetError(err error) {
-	tcs.once.Do(func() {
-		tcs.err = err
-		close(tcs.responseChan)
-		close(tcs.setCalled)
-	})
+	if !tcs.TrySetError(err) {
+		panic("TaskCompletionSource has already been completed")
+	}
 }
 
 func (tcs *TaskCompletionSource[T]) SetCanceled() {
-	tcs.Cancel()
-	tcs.SetError(context.Canceled)
+	if !tcs.TrySetCanceled() {
+		panic("TaskCompletionSource has already been completed")
+	}
 }
 
-func (tcs *TaskCompletionSource[T]) Cancel() {
+func (tcs *TaskCompletionSource[T]) TrySetResult(result T) bool {
+	completed := false
 	tcs.once.Do(func() {
-		tcs.cancelFunc()
-		close(tcs.responseChan)
-		close(tcs.setCalled)
-		tcs.err = context.Canceled
+		tcs.result = result
+		close(tcs.done)
+		completed = true
 	})
+	return completed
+}
+
+func (tcs *TaskCompletionSource[T]) TrySetError(err error) bool {
+	completed := false
+	tcs.once.Do(func() {
+		tcs.err = err
+		close(tcs.done)
+		completed = true
+	})
+	return completed
+}
+
+func (tcs *TaskCompletionSource[T]) TrySetCanceled() bool {
+	completed := false
+	tcs.once.Do(func() {
+		tcs.err = context.Canceled
+		if tcs.cancelFunc != nil {
+			tcs.cancelFunc()
+		}
+		close(tcs.done)
+		completed = true
+	})
+	return completed
 }
 
 func (tcs *TaskCompletionSource[T]) Result() (T, error) {
 	var zero T
 	select {
-	case val, ok := <-tcs.responseChan:
-		if !ok {
+	case <-tcs.done:
+		if tcs.err != nil {
 			return zero, tcs.err
 		}
-		return val, nil
+		return tcs.result, nil
 	case <-tcs.ctx.Done():
 		return zero, tcs.ctx.Err()
 	}
@@ -86,35 +108,9 @@ func (tcs *TaskCompletionSource[T]) Result() (T, error) {
 
 func (tcs *TaskCompletionSource[T]) IsCompleted() bool {
 	select {
-	case <-tcs.setCalled:
+	case <-tcs.done:
 		return true
 	default:
 		return false
 	}
-}
-
-func (tcs *TaskCompletionSource[T]) TrySetResult(result T) bool {
-	called := false
-	tcs.once.Do(func() {
-		tcs.responseChan <- result
-		close(tcs.setCalled)
-		called = true
-	})
-	return called
-}
-
-func (tcs *TaskCompletionSource[T]) TrySetError(err error) bool {
-	called := false
-	tcs.once.Do(func() {
-		tcs.err = err
-		close(tcs.responseChan)
-		close(tcs.setCalled)
-		called = true
-	})
-	return called
-}
-
-func (tcs *TaskCompletionSource[T]) TrySetCanceled() bool {
-	tcs.Cancel()
-	return tcs.TrySetError(context.Canceled)
 }
