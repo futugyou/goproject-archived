00da93079d8cca6c5553e61484b88cba0c133221
futugyou <17871902+futugyou@users.noreply.github.com>
2026-09-03 21:10:10 +0800

    Added http repsonse helper method

---
+++ openclaw/util/http.go
@@ -0,0 +1,36 @@
+package util
+
+import (
+	"net/http"
+	"strconv"
+	"time"
+)
+
+func GetRetryAfter(resp *http.Response, defaultDuration time.Duration) time.Duration {
+	if resp == nil {
+		return defaultDuration
+	}
+
+	retryHeader := resp.Header.Get("Retry-After")
+	if retryHeader == "" {
+		return defaultDuration
+	}
+
+	// 1. 尝试按秒数解析 (如 "120")
+	if seconds, err := strconv.Atoi(retryHeader); err == nil {
+		return time.Duration(seconds) * time.Second
+	}
+
+	// 2. 尝试按 HTTP 日期格式 (RFC1123) 解析 (如 "Fri, 31 Dec 2026 23:59:59 GMT")
+	if targetTime, err := http.ParseTime(retryHeader); err == nil {
+		duration := time.Until(targetTime)
+		if duration > 0 {
+			return duration
+		}
+		// 如果指定的时间已经过去了，返回 0 或者默认值
+		return 0
+	}
+
+	// 3. 解析失败，使用默认值
+	return defaultDuration
+}
